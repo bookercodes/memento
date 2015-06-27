243103912a582954f307149a51c1787e17744740
Alex Booker <alexbooker@fastmail.im>
2015-06-27 16:11:12 +0100

    reduced line width to 80 characters maximum

--- test/test.js
+++ test/test.js
@@ -1,97 +1,116 @@
-var should = require('chai').should();
-var sinon = require("sinon");
 var moment = require("moment");
+var sinon  = require("sinon");
+var chai   = require("chai");
+var should = chai.should();
 
 var PasswordReboot = require("../lib/index");
 
-describe("PasswordReboot", function() {
+describe("createToken", function() {
+  it("should return a token", function() {
+    var sut = new PasswordReboot("t9m0HLkdEyWQ6XN");
+    var user = {
+      username: "bob@hotmail.com"
+    };
 
-  describe("createToken", function() {
-    it("should return a token", function() {
-      var sut = new PasswordReboot("t9m0HLkdEyWQ6XN");
-      sut.DELIMITER = "cheese";
-      var user = {
-        username: "bob@hotmail.com"
-      };
+    var actual = sut.createToken(user);
+    console.log(actual);
 
-      var actual = sut.createToken(user);
-      console.log(actual);
+    should.exist(actual);
+  });
+});
 
-      should.exist(actual);
-    });
+describe("verifyToken", function() {
+  it("should succeed if token is correct", function() {
+    var sut = new PasswordReboot("t9m0HLkdEyWQ6XN");
+    var user = {
+      username: "bob@hotmail.com"
+    };
+    var token = sut.createToken(user);
+
+    var actual = sut.verifyToken(user, token);
+
+    actual.should.equal(true);
   });
 
-  describe("verifyToken", function() {
-    it("should succeed if token is correct", function() {
+  it("should fail if username changes", function() {
+    var sut = new PasswordReboot("t9m0HLkdEyWQ6XN");
+    var user = {
+      username: "eve@hotmail.com"
+    };
+    var token = sut.createToken(user);
+    user.username = "bob@hotmail.com";
+
+    var actual = sut.verifyToken(user, token);
+
+    actual.should.equal(false);
+  });
+
+  it("should fail if any user property changes", function() {
+    var sut = new PasswordReboot("t9m0HLkdEyWQ6XN");
+    var user = {
+      username: "eve@hotmail.com",
+      salt: "23423412341233"
+    };
+    var token = sut.createToken(user);
+    user.salt = "8888888";
+
+    var actual = sut.verifyToken(user, token);
+
+    actual.should.equal(false);
+  });
+
+  it("should fail if token has changed", function() {
+    var passwordReboot = new PasswordReboot("t9m0HLkdEyWQ6XN");
+    var user = {
+      username: "bob@hotmail.com"
+    };
+    var token = passwordReboot.createToken(user);
+    var sut = new PasswordReboot("0qNR4pkBYA");
+
+    var actual = sut.verifyToken(user, token);
+
+    actual.should.equal(false);
+  });
+
+  it("should fail if token has been tampered with", function() {
+    var sut = new PasswordReboot("t9m0HLkdEyWQ6XN");
+    var user = {
+      username: "bob@hotmail.com"
+    };
+
+    var actual = sut.verifyToken(user, "some invalid token...");
+
+    actual.should.equal(false);
+  });
+
+  it("should fail if token has expired", function() {
+    var sut = new PasswordReboot("t9m0HLkdEyWQ6XN");
+    var user = {
+      username: "bob@hotmail.com"
+    };
+    var token = sut.createToken(user);
+    var clock = sinon.useFakeTimers(moment().add({
+      minutes: 20
+    }).valueOf());
+
+    var actual = sut.verifyToken(user, token);
+
+    actual.should.equal(false);
+
+    clock.reset();
+  });
+
+  it("should fail if token has expired custom minutes until expiration",
+    function() {
       var sut = new PasswordReboot("t9m0HLkdEyWQ6XN");
       var user = {
         username: "bob@hotmail.com"
       };
-      var token = sut.createToken(user);
-
-      var actual = sut.verifyToken(user, token);
-
-      actual.should.equal(true);
-    });
-
-    it("should fail if username changes", function() {
-      var sut = new PasswordReboot("t9m0HLkdEyWQ6XN");
-      var user = {
-        username: "eve@hotmail.com"
-      };
-      var token = sut.createToken(user);
-      user.username = "bob@hotmail.com";
-
-      var actual = sut.verifyToken(user, token);
-
-      actual.should.equal(false);
-    });
-
-    it("should fail if any user property changes", function() {
-      var sut = new PasswordReboot("t9m0HLkdEyWQ6XN");
-      var user = {
-        username: "eve@hotmail.com",
-        salt: "23423412341233"
-      };
-      var token = sut.createToken(user);
-      user.salt = "8888888";
-
-      var actual = sut.verifyToken(user, token);
-
-      actual.should.equal(false);
-    });
-
-    it("should fail if token has changed", function() {
-      var passwordReboot = new PasswordReboot("t9m0HLkdEyWQ6XN");
-      var user = {
-        username: "bob@hotmail.com"
-      };
-      var token = passwordReboot.createToken(user);
-      var sut = new PasswordReboot("0qNR4pkBYA");
-
-      var actual = sut.verifyToken(user, token);
-
-      actual.should.equal(false);
-    });
-
-    it("should fail if token has been tampered with", function() {
-      var sut = new PasswordReboot("t9m0HLkdEyWQ6XN");
-      var user = {
-        username: "bob@hotmail.com"
-      };
-
-      var actual = sut.verifyToken(user, "some invalid token...");
-
-      actual.should.equal(false);
-    });
-
-    it("should fail if token has expired", function() {
-      var sut = new PasswordReboot("t9m0HLkdEyWQ6XN");
-      var user = {
-        username: "bob@hotmail.com"
-      };
-      var token = sut.createToken(user);
-      var clock = sinon.useFakeTimers(moment().add({ minutes: 20 }).valueOf());
+      var MINUTES_UNTIL_EXPIRATION = 10;
+      var token = sut.createToken(user, MINUTES_UNTIL_EXPIRATION);
+      var clock = sinon.useFakeTimers(moment().add({
+        minutes: MINUTES_UNTIL_EXPIRATION
+      }).valueOf());
 
       var actual = sut.verifyToken(user, token);
 
@@ -100,24 +119,9 @@ describe("PasswordReboot", function() {
       clock.reset();
     });
 
-    it("should fail if token has expired custom minutes until expiration", function() {
-      var sut = new PasswordReboot("t9m0HLkdEyWQ6XN");
-      var user = {
-        username: "bob@hotmail.com"
-      };
-      var MINUTES_UNTIL_EXPIRATION = 10;
-      var token = sut.createToken(user, MINUTES_UNTIL_EXPIRATION);
-      var clock = sinon.useFakeTimers(moment().add({ minutes: MINUTES_UNTIL_EXPIRATION }).valueOf());
 
-      var actual = sut.verifyToken(user, token);
-
-      actual.should.equal(false);
-
-      clock.reset();
-    });
-
-
-    it("should succeed if token has not expired custom minutes until expiration", function() {
+  it("should succeed if token has not expired custom minutes until expiration",
+    function() {
       var sut = new PasswordReboot("t9m0HLkdEyWQ6XN");
       var user = {
         username: "bob@hotmail.com"
@@ -130,23 +134,19 @@ describe("PasswordReboot", function() {
       actual.should.equal(true);
     });
 
-    it("should fail if expiration timestamp has been tampered with", function() {
-      var sut = new PasswordReboot("t9m0HLkdEyWQ6XN");
-      var user = {
-        username: "bob@hotmail.com"
-      };
-      var token = sut.createToken(user);
-      var parts = token.split(sut._DELIMITER);
-      var hmac = parts[1];
-      var expirationTime = Number.MAX_VALUE;
-      token = expirationTime + sut._DELIMITER + hmac;
+  it("should fail if expiration timestamp has been tampered with", function() {
+    var sut = new PasswordReboot("t9m0HLkdEyWQ6XN");
+    var user = {
+      username: "bob@hotmail.com"
+    };
+    var token = sut.createToken(user);
+    var parts = token.split(sut._DELIMITER);
+    var hmac = parts[1];
+    var expirationTime = Number.MAX_VALUE;
+    token = expirationTime + sut._DELIMITER + hmac;
 
-      var actual = sut.verifyToken(user, token);
+    var actual = sut.verifyToken(user, token);
 
-      actual.should.equal(false);
-    });
-
-
+    actual.should.equal(false);
   });
-
 });
